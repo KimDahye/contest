b0aaf4c2e426a1811dbd7bee8f83acf6983539eb
KimDahye <dahye.sophie.kim@gmail.com>
2017-01-02 15:17:03 +0900

    sample code added

---
+++ codershigh/src/solve/Main.java
@@ -0,0 +1,24 @@
+package solve;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sophie
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+        Main main = new Main();
+        main.solve();
+    }
+
+    public void solve() {
+        //parsing
+        Scanner scan = new Scanner(System.in);
+        int testNum = scan.nextInt();
+        for(int i = 0; i<testNum; i++) {
+           
+        }// test case End
+    }// solve method End
+}
