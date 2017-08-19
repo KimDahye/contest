ff7c719efbd00c6c9690de9fd3ffe5d20ebf9906
KimDahye <dahye.sophie.kim@gmail.com>
2017-08-19 17:36:14 +0900

    backjoon 10800 문제 - color_size_dic 메모리 너무 크게 잡기 때문에 삭제

--- python/10800.py
+++ python/10800.py
@@ -4,7 +4,7 @@ from sys import stdin
 
 
 max_size = 2000
-# 1 <= color <= len(balls) 
+# 1 <= color <= len(balls)
 # 1 <= size <= 2000
 def print_sum_of_eatable_color_ball():
     balls = int(stdin.readline())
@@ -21,7 +21,7 @@ def print_sum_of_eatable_color_ball():
             'index': i,
             'color': color,
             'size': size,
-            'accumulated_size': 0
+            'eatable_size': 0,
         }
         colorball_list.append(dic)
 
@@ -33,24 +33,24 @@ def print_sum_of_eatable_color_ball():
     # 색깔 그룹에서 size보다 작은 size들 더해놓은 값
     sorted_by_color_size = sorted(colorball_list, key=lambda k: (k['color'], k['size']))
 
-    color_size_dic = {}
     is_new_color_start = True
     for i in range(0, len(sorted_by_color_size)):
+        index = sorted_by_color_size[i]['index']
         if is_new_color_start:
-            new_color = sorted_by_color_size[i]['color']
             first_size = sorted_by_color_size[i]['size']
-            color_size_dic[new_color] = {
-                first_size : 0
-            }
+            colorball_list[index]['eatable_size'] = sum_of_smaller_size[first_size] # (- 0) 이므로 생략
             accumulated_prev_size = first_size
+            prev_same_color_eatable_size = 0
         else:
             pre_size = sorted_by_color_size[i-1]['size']
             cur_size = sorted_by_color_size[i]['size']
-            cur_color = sorted_by_color_size[i]['color']
             # 사이즈 새롭게 시작
             if pre_size != cur_size:
-                color_size_dic[cur_color][cur_size] = color_size_dic[new_color][pre_size] + accumulated_prev_size
+                same_color_eatable_size = prev_same_color_eatable_size + accumulated_prev_size
+                eatable_size = sum_of_smaller_size[cur_size] - same_color_eatable_size
+                colorball_list[index]['eatable_size'] = eatable_size
                 accumulated_prev_size = cur_size
+                prev_same_color_eatable_size = same_color_eatable_size
             # 사이즈 계속 이어짐
             else:
                 accumulated_prev_size = accumulated_prev_size + cur_size
@@ -60,12 +60,9 @@ def print_sum_of_eatable_color_ball():
             ne = sorted_by_color_size[i + 1]
             is_new_color_start = cur['color'] != ne['color']
 
-    # sum_of_smaller_size[cur_size] - sum_of_smaller_size_of_color()
+    # print
     for e in colorball_list:
-        cur_color = e['color']
-        cur_size = e['size']
-        eatable_size = sum_of_smaller_size[cur_size] - color_size_dic[cur_color][cur_size]
-        print(eatable_size)
+        print(e['eatable_size'])
 
     return
 
