14b7dfd7d3d9ac6a4bfcfcabc8263861d0dc86bc
KimDahye <dahye.sophie.kim@gmail.com>
2017-08-19 17:01:49 +0900

    backjoon 10800 문제 - 지난 커밋의 문제(color의 범위가 [1:len(balls)]인데, [1:4]로 생각) 수정 - 하지만 backjoon에서 여전히 런타임 나는 상태

--- python/10800.py
+++ python/10800.py
@@ -1,82 +1,72 @@
 # -*- coding: utf-8 -*-
+# prob_url: https://www.acmicpc.net/problem/10800
 from sys import stdin
 
 
+max_size = 2000
+# 1 <= color <= len(balls) 
+# 1 <= size <= 2000
 def print_sum_of_eatable_color_ball():
     balls = int(stdin.readline())
 
     # initialize
     colorball_list = []
+    accumulated_size = [0]*max_size
     for i in range(0, balls):
         args = [int(e) for e in stdin.readline().split()]
+        color = args[0]
+        size = args[1]
+        accumulated_size[size] = accumulated_size[size] + size
         dic = {
             'index': i,
-            'color': args[0],
-            'size': args[1],
-            'sumOfColor1': 0,
-            'sumOfColor2': 0,
-            'sumOfColor3': 0,
-            'sumOfColor4': 0,
-            'sumOfEatable': 0
+            'color': color,
+            'size': size,
+            'accumulated_size': 0
         }
         colorball_list.append(dic)
 
-    # sort by size
-    sorted_by_size = sorted(colorball_list, key=lambda k: k['size'], reverse=True)
+    # get list of sum_of_smaller_size
+    sum_of_smaller_size = [0]*max_size
+    for i in range(1, max_size):
+        sum_of_smaller_size[i] = sum_of_smaller_size[i-1] + accumulated_size[i-1]
 
-    # calculate sumOfColr1,2,3,4
-    first_color = sorted_by_size[0]['color']
-    sorted_by_size[0]['sumOfColor'+str(first_color)] = sorted_by_size[0]['size']
-    for i in range(1, len(sorted_by_size)):
-        pre = sorted_by_size[i-1]
-        cur = sorted_by_size[i]
+    # 색깔 그룹에서 size보다 작은 size들 더해놓은 값
+    sorted_by_color_size = sorted(colorball_list, key=lambda k: (k['color'], k['size']))
 
-        # 이전꺼 반영
-        cur['sumOfColor1'] = pre['sumOfColor1']
-        cur['sumOfColor2'] = pre['sumOfColor2']
-        cur['sumOfColor3'] = pre['sumOfColor3']
-        cur['sumOfColor4'] = pre['sumOfColor4']
+    color_size_dic = {}
+    is_new_color_start = True
+    for i in range(0, len(sorted_by_color_size)):
+        if is_new_color_start:
+            new_color = sorted_by_color_size[i]['color']
+            first_size = sorted_by_color_size[i]['size']
+            color_size_dic[new_color] = {
+                first_size : 0
+            }
+            accumulated_prev_size = first_size
+        else:
+            pre_size = sorted_by_color_size[i-1]['size']
+            cur_size = sorted_by_color_size[i]['size']
+            cur_color = sorted_by_color_size[i]['color']
+            # 사이즈 새롭게 시작
+            if pre_size != cur_size:
+                color_size_dic[cur_color][cur_size] = color_size_dic[new_color][pre_size] + accumulated_prev_size
+                accumulated_prev_size = cur_size
+            # 사이즈 계속 이어짐
+            else:
+                accumulated_prev_size = accumulated_prev_size + cur_size
 
-        # 이번꺼 누적
-        cur_color = cur['color']
-        color_key = 'sumOfColor' + str(cur_color)
-        cur[color_key] = pre[color_key] + cur['size']
+        if i != len(sorted_by_color_size)-1:
+            cur = sorted_by_color_size[i]
+            ne = sorted_by_color_size[i + 1]
+            is_new_color_start = cur['color'] != ne['color']
 
-    # calculate sumOfEatable
-    last = sorted_by_size[-1]
-    first_sum_of_eatable = get_sum_of_eatable(first_color, last)
-    sorted_by_size[0]['sumOfEatable'] = first_sum_of_eatable
-
-    for i in range(1, len(sorted_by_size)):
-        pre = sorted_by_size[i-1]
-        cur = sorted_by_size[i]
-        cur_color = cur['color']
-        sum_of_eatable = get_sum_of_eatable(cur_color, last, pre)
-        sorted_by_size[i]['sumOfEatable'] = sum_of_eatable
-
-    # sort by index and print result
-    sorted_by_index = sorted(sorted_by_size, key=lambda k: k['index'])
-    for e in sorted_by_index:
-        print(e['sumOfEatable'])
+    # sum_of_smaller_size[cur_size] - sum_of_smaller_size_of_color()
+    for e in colorball_list:
+        cur_color = e['color']
+        cur_size = e['size']
+        eatable_size = sum_of_smaller_size[cur_size] - color_size_dic[cur_color][cur_size]
+        print(eatable_size)
 
     return
-
-
-def get_sum_of_eatable(cur_color, last, pre=None):
-    if pre is None:
-        pre = {
-            'sumOfColor1': 0,
-            'sumOfColor2': 0,
-            'sumOfColor3': 0,
-            'sumOfColor4': 0,
-        }
-
-    sum_of_eatable = 0
-    for color in range(1, 5):
-        if cur_color == color:
-            continue
-        color_key = 'sumOfColor' + str(color)
-        sum_of_eatable = sum_of_eatable + (last[color_key] - pre[color_key])
-    return sum_of_eatable
 
 print_sum_of_eatable_color_ball()
